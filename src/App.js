a44abbd83ef347c9075594ee02e843fb430e8839
Jayson <jayson.lewis5@gmail.com>
2021-12-04 15:40:40 -0600

    Working db seeding

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 
 import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
 import './App.css';
 import './components/UserProfile';
 import './components/MomentsMaster';
-import { InMemoryCache } from "@apollo/client";
+import { ApolloProvider, InMemoryCache } from "@apollo/client";
 import MomentsMaster from "./components/MomentsMaster";
-import SeedMomes from "./components/momentSeed";
+import SeedMoments from "./components/SeedMoments";
 import SeedSets from "./components/setSeed";
 import {React, useState, useEffect} from 'react';
 import axios from 'axios';
+import GetMoments from './components/GetMoments';
 
 
 
@@ -44,7 +44,11 @@ const client = new ApolloClient({
     <ApolloProvider client={client}>
       <div>
         <h1>Testing Graphql for Topshots!</h1>
-        <SeedMomes sets={sets}/>
+        {sets.length > 0 ? 
+          <GetMoments sets={sets}/>
+          : <div>Loading</div>          
+        }
+           
       </div>
     </ApolloProvider>
   );
