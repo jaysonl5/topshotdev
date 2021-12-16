ff02d2998c831fa5ff74b954da6e77ff7775a4e7
Jayson <jayson.lewis5@gmail.com>
2021-12-16 13:24:36 -0600

    Updates server.js to reflect re-structured client

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "client", "public")));
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, "client", "public", "index.html"));
